a0cd657258ed3342a435f35d9421c4a3328b6c00
prabhu <prabhukadode1@gmail.com>
2025-12-03 18:28:55 +0530

    line graph and subplot

---
+++ subplot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fix,axis = plt.subplots(1,2,figsize=(8,5))
+
+axis[1].set_title("line")
+axis[1].bar([1,2,3,4,5,6],[2,4,6,8,10,12],color="red")
+axis[1].set_ylabel("Y albesl")
+axis[1].set_xlabel("x label")
+
+axis[0].set_title("line")
+axis[0].plot([1,2,3,4,5,6],[2,4,6,8,10,12],marker="o",linestyle="-",color="red")
+axis[0].set_ylabel("Y albesl")
+axis[0].set_xlabel("x label")
+plt.show()
