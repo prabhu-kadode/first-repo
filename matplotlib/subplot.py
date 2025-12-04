b7da95853c82052323a765da367fb28d4622390a
prabhu <prabhukadode1@gmail.com>
2025-12-04 17:44:01 +0530

    subplot

--- matplotlib/subplot.py
+++ matplotlib/subplot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
 fix,axis = plt.subplots(1,2,figsize=(8,5))
-
+colors = ['red','green','blue','skyblue','yellow','black']
 axis[1].set_title("line")
-axis[1].bar([1,2,3,4,5,6],[2,4,6,8,10,12],color="red")
+axis[1].bar([1,2,3,4,5,6],[2,4,6,8,10,12],color=colors)
 axis[1].set_ylabel("Y albesl")
 axis[1].set_xlabel("x label")
 
